30505011dc71fb18712369a94c920fd3fedd99fe
notayslo <antonsipaylo@gmail.com>
2019-05-31 20:35:28 +0300

    Initial commit

---
+++ Lab10/SignInTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SignInTableViewCell.swift
+//  Lab10
+//
+//  Created by Anton Sipaylo on 5/31/19.
+//  Copyright © 2019 Anton Sipaylo. All rights reserved.
+//
+
+import UIKit
+
+class SignInTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Lab10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab10
+//
+//  Created by Anton Sipaylo on 5/31/19.
+//  Copyright © 2019 Anton Sipaylo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
